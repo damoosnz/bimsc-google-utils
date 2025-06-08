4a207c9535a2861ac8312d554532de234f10d230
damoosnz <damien.legrand@bimsolutionscentre.com>
2025-06-09 09:14:38 +1200

    added the init function to the export for custom case where the drive api neeed to be used outside of thescoped functions

--- gdrive/renders/drive-render.js
+++ gdrive/renders/drive-render.js
@@ -2,11 +2,12 @@ import findFileIdInDrive from "../functions/find-file-id-in-drive.js"
 import updateFileInDrive from "../functions/update-file-in-drive.js"
 import uploadFileToDrive from "../functions/upload-file-to-drive.js"
 import { uploadOverwriteFileInDrive } from "../functions/overwrite-file-in-drive.js"
-
+import initDriveClient from "../init/initDriveClient.js"
 
 export const drive = {
     findFileId: findFileIdInDrive,
     uploadFile: uploadFileToDrive,
     updateFile: updateFileInDrive,
     overwriteFile: uploadOverwriteFileInDrive,
+    initDriveClient: initDriveClient,
 }
