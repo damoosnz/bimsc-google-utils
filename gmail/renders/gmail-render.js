3e27b9b9ccaddb3d652840761b7e9485c16720b9
damoosnz <damien.legrand@bimsolutionscentre.com>
2025-06-14 12:09:39 +1200

    redidi the import statemnt into the gmail object

--- gmail/renders/gmail-render.js
+++ gmail/renders/gmail-render.js
@@ -1,7 +1,7 @@
 import sendEmail from "../functions/send-email.js";
 import getEmail from "../functions/get-email.js";
 import listEmails from "../functions/list-emails.js";
-import initGmailClient from "../init/initGmailClient.js";
+import initGmailClient from "../init/init-gmail-client.js";
 
 export const gmail = {
     sendEmail,
